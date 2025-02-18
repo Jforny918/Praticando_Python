308494f75b61a152725cf5d3a4df379112765354
Júlia Forny de Souza Muniz <forny918@gmail.com>
2025-02-18 17:16:44 -0300

    Create ex010.py

---
+++ praticando/ex010.py
@@ -0,0 +1,8 @@
+print ('======DESAFIO 10======')
+#Crie um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+n1=float(input('Digite um valor em metros: '))
+c=n1*100 #centímetros
+m=n1/1000 #milímetros
+print ('{} metros equivale a {} centímetros e {} milímetros'.format(n1,c,m))
+               
+ 
