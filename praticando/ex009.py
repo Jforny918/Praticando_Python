42e005a286b6fd941ede3bc8c24fdf7fb5bb7fc4
Júlia Forny de Souza Muniz <forny918@gmail.com>
2025-02-18 17:16:41 -0300

    Create ex009.py

---
+++ praticando/ex009.py
@@ -0,0 +1,6 @@
+print ('======DESAFIO 09======')
+#Faça um programa que leia a primeira nota de um aluno e a segunda nota e mostre a média.
+n1=int(input("Digite a primeira nota:"))
+n2=int(input("Digite a segunda nota:"))
+m=(n1+n2)/2
+print ('A média entre {} e {} é {}'.format(n1,n2,m))
