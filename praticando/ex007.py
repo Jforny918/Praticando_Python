9d236467bf9fbd59e5856eae153e6fecf719e497
Júlia Forny de Souza Muniz <forny918@gmail.com>
2025-02-18 17:16:33 -0300

    Create ex007.py

---
+++ praticando/ex007.py
@@ -0,0 +1,4 @@
+print ('======DESAFIO 07======') 
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor.
+n1=int(input('Digite um número: '))
+print ('O sucessor de {} é {} e o antecessor é {}'.format(n1, n1+1, n1-1))
