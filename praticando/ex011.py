f6f5fcbd391df151b086c84b478ad319db85df33
Júlia Forny de Souza Muniz <forny918@gmail.com>
2025-02-18 17:16:47 -0300

    Create ex011.py

---
+++ praticando/ex011.py
@@ -0,0 +1,13 @@
+print ('======DESAFIO 11======')
+n1=int(input('Qual número você quer saber a tabuada? '))
+n1x1=n1*1
+n1x2=n1*2
+n1x3=n1*3
+n1x4=n1*4
+n1x5=n1*5
+n1x6=n1*6
+n1x7=n1*7
+n1x8=n1*8
+n1x9=n1*9
+n1x10=n1*10
+print ('A tabuada de {} é: \n {} x 1 = {} \n {} x 2 = {} \n {} x 3 = {} \n {} x 4 = {} \n {} x 5 = {} \n {} x 6 = {} \n {} x 7 = {} \n {} x 8 = {} \n {} x 9 = {} \n {} x 10 = {} \n'.format (n1,n1,n1x1,n1,n1x2,n1,n1x3,n1,n1x4,n1,n1x5,n1,n1x6,n1,n1x7,n1,n1x8,n1,n1x9,n1,n1x10))
