7b4a498970d4dfa626212c0463a717f095dd69d5
Júlia Forny de Souza Muniz <forny918@gmail.com>
2025-02-18 22:00:24 -0300

    Update ex013.py

--- praticando/ex013.py
+++ praticando/ex013.py
@@ -4,4 +4,4 @@ l=float(input("Digite a largura do terreno: "))
 c=float(input('Digite o comprimento do terreno: '))
 a=l*c
 t=a/2
-print ('a área do terreno é {} m² e a quantidade de tinta necessária para pintá-lo é {} litros'.format(a,t))
+print ('A área do terreno é {} m² e a quantidade de tinta necessária para pintá-lo é {} litros'.format(a,t))
