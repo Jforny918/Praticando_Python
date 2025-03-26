318f9d745953e70dfa2ccaff9176f067237a3549
Júlia Forny de Souza Muniz <forny918@gmail.com>
2025-03-26 15:22:15 -0300

    ajuste de erros

--- estrutura de repetição while/ex18.py
+++ estrutura de repetição while/ex18.py
@@ -1,12 +1,12 @@
 from random import randint
-
+v = 0
 while True:
     usuario = int (input ('Digite um valor de 0 a 10: '))
     computador = randint (0, 11)
     total = usuario + computador
     tipo = ' '
     while tipo not in 'PpIi':
-        tipo = input ('Par ou ímpar? [P/I]').strip().upper()[0]
+        tipo = input ('Par ou ímpar? [P/I] ').strip().upper()[0]
     print (f'Você jogou {usuario} e o computador jogou {computador}. Total de {total}')
     if tipo == 'P':
         if total % 2 == 0:
@@ -23,5 +23,5 @@ while True:
             print ('Você PERDEU!')
             break
         print ('Vamos jogar novamente...')
-    print ('Game Over! Você venceu {} vezes!')
+print (f'Game Over! Você venceu {v} vezes!')
 
