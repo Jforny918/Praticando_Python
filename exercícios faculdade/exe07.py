9a0966fd4a96578e07b9dbaf79bb7458b34d0452
Júlia Forny de Souza Muniz <forny918@gmail.com>
2025-10-06 10:01:28 -0300

    exercicios aula python

---
+++ exercícios faculdade/exe07.py
@@ -0,0 +1,7 @@
+celsius = float (input ("Digite a temperatura em Celsius: "))
+fahrenheit = (celsius  * 1.8 + 32)
+print ("A temperatura °F é: {:.2f}".format(fahrenheit))
+kelvin = celsius + 273.15
+print ("A temperatura K é: {:.2f}".format(kelvin))
+
+
